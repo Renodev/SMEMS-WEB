15470409dfb36081b313a15e6c442e756f2d0be1
kaungsithu <kaungsithu@localhost>
2020-12-09 21:19:44 +0630

    Kaung Si Thu - Material Service Add

---
+++ SMEMS-WEB/src/main/java/com/rnc/smems/web/services/MaterialService.java
@@ -0,0 +1,40 @@
+package com.rnc.smems.web.services;
+
+import java.util.List;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import com.rnc.smems.web.entities.Material;
+import com.rnc.smems.web.repositories.MaterialRepository;
+
+@LocalBean
+@Stateless
+public class MaterialService {
+	
+	@Inject
+	private MaterialRepository materialRepository;
+
+	public void save (Material material) {
+		if (material.getId() == 0) {
+			materialRepository.save(material);
+		} else {
+			materialRepository.update(material);
+		}
+	}
+	
+	public void delete (Material material) {
+		Material mtr = findByID(material.getId());
+		materialRepository.delete(mtr);
+	}
+	
+	public Material findByID (long id) {
+		return materialRepository.findByID(id);
+	}
+	
+	public List<Material> findAll () {
+		return materialRepository.findAll();
+	}
+	
+}
